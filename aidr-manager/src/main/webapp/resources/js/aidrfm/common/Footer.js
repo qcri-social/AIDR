c7550ec18cc3089c36b2be0cc3b3ea60a842f81b
kirkh <alex.mikhashchuk@gmail.com>
2013-12-20 19:59:37 +0200

    aidr-manager: Footer changes - Add Terms of service link

--- aidr-manager/src/main/webapp/resources/js/aidrfm/common/Footer.js
+++ aidr-manager/src/main/webapp/resources/js/aidrfm/common/Footer.js
@@ -3,6 +3,17 @@ Ext.define('AIDRFM.common.Footer', {
     alias: 'widget.aidr-footer',
 
     width: '100%',
-    html: '<div class="site-footer"><div class="footer"><a style="text-decoration: none;color: #ffffff" href="http://www.qcri.qa/">A project by <img align="middle" id="footerqcrilogo" src="' + BASE_URL + '/resources/img/qcri-gray-horiz.png"></a></div>' +
+    html: '<div class="site-footer">' +
+        '<div class="footer">' +
+
+        '<div style="float: right;">' +
+        '<a style="text-decoration: none; color: #ffffff; font-size: 13px;" href="http://www.qcri.qa/">A project by <img align="middle" id="footerqcrilogo" src="' + BASE_URL + '/resources/img/qcri-gray-horiz.png"/></a>' +
+        '</div>' +
+
+        '<div style="float: left; padding-top: 16px;">' +
+        '<a style="text-decoration: underline; color: #ffffff; font-size: 13px;" href="http://aidr.qcri.org/tos/">Terms of Service</a>' +
+        '</div>' +
+
+        '</div>' +
         '</div>'
 });
