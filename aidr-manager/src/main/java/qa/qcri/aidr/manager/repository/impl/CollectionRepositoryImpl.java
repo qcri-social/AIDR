95aa8904c589430a75f7c491b3bf1de57f773acd
jikimlucas <jikimlucas@me.com>
2014-05-05 11:56:43 +0300

    #70227314, The top collections in the My Collections page should be those that have just been created

--- aidr-manager/src/main/java/qa/qcri/aidr/manager/repository/impl/CollectionRepositoryImpl.java
+++ aidr-manager/src/main/java/qa/qcri/aidr/manager/repository/impl/CollectionRepositoryImpl.java
@@ -1,5 +1,6 @@
 package qa.qcri.aidr.manager.repository.impl;
 
+import org.apache.commons.collections.ListUtils;
 import org.hibernate.*;
 import org.hibernate.criterion.*;
 import org.springframework.orm.hibernate3.HibernateCallback;
@@ -15,6 +16,7 @@ import java.io.Serializable;
 import java.math.BigInteger;
 import java.net.URLDecoder;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
@@ -46,24 +48,55 @@ public class CollectionRepositoryImpl extends GenericRepositoryImpl<AidrCollecti
                         " WHERE (c.status != :statusValue AND (c.user_id =:userId OR c_m.id_manager = :userId)) " +
                         " order by c.startDate DESC, c.createdDate DESC LIMIT :start, :limit ";
 
+
+
+                String sql2 = " SELECT DISTINCT c.id FROM AIDR_COLLECTION c " +
+                        " LEFT OUTER JOIN AIDR_COLLECTION_TO_MANAGER c_m " +
+                        " ON c.id = c_m.id_collection " +
+                        " WHERE (c.startDate IS NULL and c.status != :statusValue AND (c.user_id =:userId OR c_m.id_manager = :userId)) " +
+                        " order by c.createdDate DESC LIMIT :start, :limit ";
+
                 SQLQuery sqlQuery = session.createSQLQuery(sql);
                 sqlQuery.setParameter("userId", userId);
                 sqlQuery.setParameter("start", start);
                 sqlQuery.setParameter("limit", limit);
                 sqlQuery.setParameter("statusValue", CollectionStatus.TRASHED.ordinal());
                 List<Integer> ids = (List<Integer>) sqlQuery.list();
-                return ids != null ? ids : Collections.emptyList();
+
+                SQLQuery sqlQuery2 = session.createSQLQuery(sql2);
+                sqlQuery2.setParameter("userId", userId);
+                sqlQuery2.setParameter("start", start);
+                sqlQuery2.setParameter("limit", limit);
+                sqlQuery2.setParameter("statusValue", CollectionStatus.TRASHED.ordinal());
+                List<Integer> ids2 = (List<Integer>) sqlQuery2.list();
+
+                // first, remove duplicate
+                if(!ids.isEmpty() && ids != null){
+                    ids.removeAll(ids2) ;
+                    // add
+                    ids2.addAll(ids) ;
+                }
+
+                return ids2 != null ? ids2 : Collections.emptyList();
             }
         });
 
-        Criteria criteria = getHibernateTemplate().getSessionFactory().getCurrentSession().createCriteria(AidrCollection.class);
-        criteria.setResultTransformer(CriteriaSpecification.DISTINCT_ROOT_ENTITY);
-
-        criteria.add(Restrictions.in("id", collectionIds));
-		criteria.addOrder(Order.desc("startDate"));
-		criteria.addOrder(Order.desc("createdDate"));
-
-		return (List<AidrCollection>) criteria.list();
+       // Criteria criteria = getHibernateTemplate().getSessionFactory().getCurrentSession().createCriteria(AidrCollection.class);
+       // criteria.setResultTransformer(CriteriaSpecification.DISTINCT_ROOT_ENTITY);
+       // System.out.println("out");
+        List<AidrCollection> a = new ArrayList<AidrCollection>();
+        for(int i =0; i < collectionIds.size(); i++){
+          //  System.out.println(collectionIds.get(i));
+            AidrCollection collection =	this.findById(collectionIds.get(i));
+            a.add(collection) ;
+        }
+        // by default, criteria sort is by id asc. so, change logic to get each aidr_collection
+       // criteria.add(Restrictions.in("id", collectionIds));
+		//criteria.addOrder(Order.desc("startDate"));
+		//criteria.addOrder(Order.desc("createdDate"));
+        //List<AidrCollection> a = (List<AidrCollection>) criteria.list();
+        return a;
+		//return (List<AidrCollection>) criteria.list();
 	}
 
     @SuppressWarnings("unchecked")
