d527bd997eb7537713e6da5b86b51bc8857cbc83
jikimlucas <jikimlucas@me.com>
2014-07-24 16:00:55 +0300

    updated

--- aidr-trainer-api/src/main/java/qa/qcri/aidr/trainer/api/store/URLReference.java
+++ aidr-trainer-api/src/main/java/qa/qcri/aidr/trainer/api/store/URLReference.java
@@ -9,10 +9,10 @@ package qa.qcri.aidr.trainer.api.store;
  */
 public class URLReference {
 
-    public static String PUBLIC_LINK = "http://clickers.micromappers.org/aidr/";
+    //public static String PUBLIC_LINK = "http://clickers.micromappers.org/aidr/";
      // for direct connection to api service.
-    //public static String PUBLIC_LINK = "http://pybossa-dev.qcri.org/AIDRCrowdsourcingAPI/crisisContent.html?code=";
-     //public static String PUBLIC_LINK = "http://localhost:8081/AIDRTrainerAPI/crisisContent.html?code=";
+    public static String PUBLIC_LINK = "http://pybossa-dev.qcri.org/AIDRCrowdsourcingAPI/crisisContent.html?code=";
+   //  public static String PUBLIC_LINK = "http://94.125.228.197:1055/AIDRTrainerAPI/crisisContent.html?code=";
 
      public static String PUBLIC_PYBOSSA_URL = "http://clickers.micromappers.org";
      //http://pybossa-dev.qcri.org/
