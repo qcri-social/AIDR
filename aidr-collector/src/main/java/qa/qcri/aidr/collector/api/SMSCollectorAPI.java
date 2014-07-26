91e3dc0d49c3dd953258c1d89b9930b55d3d0b76
Eugene <eugene@firejack.net>
2014-07-26 18:45:51 +0400

    Extension of AIDR-Collector to collect SMSs

---
+++ aidr-collector/src/main/java/qa/qcri/aidr/collector/api/SMSCollectorAPI.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qa.qcri.aidr.collector.api;
+
+//import com.sun.jersey.api.client.Client;
+//import com.sun.jersey.api.client.ClientResponse;
+//import com.sun.jersey.api.client.WebResource;
+
+import org.apache.log4j.Logger;
+import qa.qcri.aidr.collector.beans.SMS;
+import qa.qcri.aidr.collector.logging.Loggable;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author Imran
+ */
+@Path("/sms")
+public class SMSCollectorAPI extends Loggable {
+
+    private static Logger logger = Logger.getLogger(SMSCollectorAPI.class.getName());
+
+    @GET
+    @Path("/start")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response startTask(@QueryParam("collection_code") String code) {
+        return Response.ok().build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/stop")
+    public Response stopTask(@QueryParam("collection_code") String code) {
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("endpoint/receive/{collection_code}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response receive(@PathParam("collection_code") String code, SMS sms) {
+        return Response.ok().build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/status/all")
+    public Response getStatusAll() {
+        return Response.ok().build();
+    }
+
+}
---
+++ aidr-collector/src/main/java/qa/qcri/aidr/collector/beans/SMS.java
@@ -0,0 +1,28 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mjr
+ * Date: 26.07.14
+ * Time: 18:19
+ */
+package qa.qcri.aidr.collector.beans;
+
+public class SMS {
+    private AIDR aidr;
+    private String text;
+
+    public AIDR getAidr() {
+        return aidr;
+    }
+
+    public void setAidr(AIDR aidr) {
+        this.aidr = aidr;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
