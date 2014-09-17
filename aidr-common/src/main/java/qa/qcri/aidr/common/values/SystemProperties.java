d8995aa4176ea762d1d4350973ad20516d30351c
Koushik Sinha <koushik.sinha@gmail.com>
2014-09-17 17:54:46 +0300

    pom update and system properties added

---
+++ aidr-common/src/main/java/qa/qcri/aidr/common/values/SystemProperties.java
@@ -0,0 +1,10 @@
+package qa.qcri.aidr.common.values;
+
+public class SystemProperties {
+	public static final String fetchMainUrl = "http://localhost:8084/AIDRCollector/webresources";
+	public static final String taggerMainUrl= "http://localhost:8084/AIDRTaggerAPI/rest";
+	public static final String persisterMainUrl = "http://localhost:8084/AIDRPersister/webresources";
+	public static final String crowdsourcingAPIMainUrl = "http://localhost:8084/AIDRTrainerAPI/rest";
+	public static final String outputAPIMainUrl = "http://localhost:8084/AIDROutput/rest";
+	public static final String serverUrl = "http://localhost/";
+}
