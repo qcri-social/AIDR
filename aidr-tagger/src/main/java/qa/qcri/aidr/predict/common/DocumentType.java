2977e987456270b0ef8be4d2d4ee88eeca64cc00
imran15 <mimran15@gmail.com>
2014-10-02 13:57:53 +0300

    Fixed docType

--- aidr-tagger/src/main/java/qa/qcri/aidr/predict/common/DocumentType.java
+++ aidr-tagger/src/main/java/qa/qcri/aidr/predict/common/DocumentType.java
@@ -3,6 +3,6 @@ package qa.qcri.aidr.predict.common;
 public class DocumentType {
 	
 	public static final String TWIITER_DOC = "twitter";
-	public static final String SMS_DOC = "SMS";
+	public static final String SMS_DOC = "sms";
 	
 }
