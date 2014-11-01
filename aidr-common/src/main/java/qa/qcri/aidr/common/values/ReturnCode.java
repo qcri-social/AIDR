b68d38079c518c888f93c46798fc3288e679467c
imran15 <mimran15@gmail.com>
2014-11-01 13:43:52 +0300

    Return codes updates

--- aidr-common/src/main/java/qa/qcri/aidr/common/values/ReturnCode.java
+++ aidr-common/src/main/java/qa/qcri/aidr/common/values/ReturnCode.java
@@ -1,11 +1,12 @@
 package qa.qcri.aidr.common.values;
 
 public enum ReturnCode {
-	FAIL(-1), SUCCESS(1);
+ERROR, WARNING, SUCCESS;	
+//ERROR(-1), WARNING(0), SUCCESS(1);
 	
-	int value;
+	//int value;
 	
-	ReturnCode(int value) {
-		this.value = value;
-	}
+//	ReturnCode(int value) {
+//		this.value = value;
+//	}
 }
