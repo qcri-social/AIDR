a3b339d98ffc2e81d986a7b84fdd4f77027c0681
imran15 <mimran15@gmail.com>
2014-11-27 11:23:00 +0300

    Added missing modelCollection, annotation, and completed DTOs

--- aidr-db-manager/src/main/java/qa/qcri/aidr/dbmanager/ejb/remote/facade/ModelFacadeRemote.java
+++ aidr-db-manager/src/main/java/qa/qcri/aidr/dbmanager/ejb/remote/facade/ModelFacadeRemote.java
@@ -6,6 +6,7 @@
 
 package qa.qcri.aidr.dbmanager.ejb.remote.facade;
 
+import java.util.List;
 import javax.ejb.Remote;
 import qa.qcri.aidr.dbmanager.dto.ModelDTO;
 
@@ -18,4 +19,7 @@ public interface ModelFacadeRemote {
     
     public ModelDTO getModelByID();
     
+    public List<ModelDTO> getModelByModelFamilyID(Integer modelFamilyID, Integer start, Integer limit);
+    
+    public List<ModelDTO> getModelByCrisisID(long crisisID);
 }
