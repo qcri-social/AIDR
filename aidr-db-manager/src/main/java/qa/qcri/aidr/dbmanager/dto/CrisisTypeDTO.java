51d4b0f78019eca9a35a4a22a891313933bf1d2e
imran15 <mimran15@gmail.com>
2014-12-23 14:38:54 +0300

    db-manager change to accomodate crises field

--- aidr-db-manager/src/main/java/qa/qcri/aidr/dbmanager/dto/CrisisTypeDTO.java
+++ aidr-db-manager/src/main/java/qa/qcri/aidr/dbmanager/dto/CrisisTypeDTO.java
@@ -11,115 +11,126 @@ import qa.qcri.aidr.dbmanager.entities.misc.Crisis;
 import qa.qcri.aidr.dbmanager.entities.misc.CrisisType;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @XmlRootElement
-@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CrisisTypeDTO implements java.io.Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 8074463052776843105L;
+    private static final long serialVersionUID = 8074463052776843105L;
 
-	@XmlElement
-	private Long crisisTypeId;
+    @XmlElement
+    private Long crisisTypeId;
 
-	@XmlElement
-	private String name;
+    @XmlElement
+    private String name;
 
-	@XmlElement
-	private List<CrisisDTO> crisisesDTO = null;
+    @XmlElement
+    private List<CrisisDTO> crisisesDTO = null;
 
-	public CrisisTypeDTO() {
-	}
+    @XmlElement
+    private int numberOfCrisisAssociated;
 
-	public CrisisTypeDTO(String name) {
-		this.name = name;
-	}
+    public CrisisTypeDTO() {
+    }
 
-	public CrisisTypeDTO(Long crisisTypeId, String name) {
-		this.setCrisisTypeId(crisisTypeId);
-		this.setName(name);
-	}
+    public CrisisTypeDTO(String name) {
+        this.name = name;
+    }
 
-	public CrisisTypeDTO(Long crisisTypeId, String name, List<CrisisDTO> crisisesDTO) {
-		this.setCrisisTypeId(crisisTypeId);
-		this.setName(name);
-		this.setCrisisesDTO(crisisesDTO);
-	}
+    public CrisisTypeDTO(Long crisisTypeId, String name) {
+        this.setCrisisTypeId(crisisTypeId);
+        this.setName(name);
+    }
 
-	public CrisisTypeDTO(CrisisType crisisType) throws PropertyNotSetException {
-		if (crisisType != null) {
-			//System.out.println("CrisisType Hash code: " + crisisType.hashCode());
-			
-			this.setCrisisTypeId(crisisType.getCrisisTypeId());
-			this.setName(crisisType.getName());
-			if (crisisType.hasCrisises()) {
-				this.setCrisisesDTO(toCrisisDTOList(crisisType.getCrisises()));
-			}
-		} else {
-			//throw new IllegalArgumentException("Cannot be null");
-			System.out.println("Entity = null in constructor");
-		}
-	}
+    public CrisisTypeDTO(Long crisisTypeId, String name, List<CrisisDTO> crisisesDTO) {
+        this.setCrisisTypeId(crisisTypeId);
+        this.setName(name);
+        this.setCrisisesDTO(crisisesDTO);
+    }
 
-	public Long getCrisisTypeId() {
-		return this.crisisTypeId;
-	}
+    public CrisisTypeDTO(CrisisType crisisType) throws PropertyNotSetException {
+        if (crisisType != null) {
+            this.setCrisisTypeId(crisisType.getCrisisTypeId());
+            this.setName(crisisType.getName());
+            if (crisisType.hasCrisises()) {
+                this.setCrisisesDTO(toCrisisDTOList(crisisType.getCrisises()));
+                this.numberOfCrisisAssociated = crisisType.getCrisises().size();
+            }
+        } else {
+            System.out.println("Entity = null in constructor");
+        }
+    }
+    
+    public Long getCrisisTypeId() {
+        return this.crisisTypeId;
+    }
 
-	public void setCrisisTypeId(Long crisisTypeId) {
-		this.crisisTypeId = crisisTypeId;
-	}
+    public void setCrisisTypeId(Long crisisTypeId) {
+        this.crisisTypeId = crisisTypeId;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public List<CrisisDTO> getCrisisesDTO() {
-		return this.crisisesDTO;
-	}
+    public List<CrisisDTO> getCrisisesDTO() {
+        return this.crisisesDTO;
+    }
 
-	public void setCrisisesDTO(List<CrisisDTO> crisisesDTO) {
-			this.crisisesDTO = crisisesDTO;
-	}
+    public void setCrisisesDTO(List<CrisisDTO> crisisesDTO) {
+        this.crisisesDTO = crisisesDTO;
+    }
 
-	private List<CrisisDTO> toCrisisDTOList(List<Crisis> list) throws PropertyNotSetException {
-		if (list != null) {
-			List<CrisisDTO> crisisesDTO = new ArrayList<CrisisDTO>();
-			for (Crisis c: list) {
-				crisisesDTO.add(new CrisisDTO(c));
-			}
-			return crisisesDTO;
-		}
-		return null;
-	}
+    private List<CrisisDTO> toCrisisDTOList(List<Crisis> list) throws PropertyNotSetException {
+        if (list != null) {
+            List<CrisisDTO> crisisesDTO = new ArrayList<CrisisDTO>();
+            for (Crisis c : list) {
+                crisisesDTO.add(new CrisisDTO(c));
+            }
+            return crisisesDTO;
+        }
+        return null;
+    }
 
-	private List<Crisis> toCrisisList(List<CrisisDTO> list) throws PropertyNotSetException {
-		if (list != null) {
-			List<Crisis> crisises = new ArrayList<Crisis>();
-			for (CrisisDTO dto: list) {
-				crisises.add(dto.toEntity());
-			}
-			return crisises;
-		}
-		return null;
-	}
+    private List<Crisis> toCrisisList(List<CrisisDTO> list) throws PropertyNotSetException {
+        if (list != null) {
+            List<Crisis> crisises = new ArrayList<Crisis>();
+            for (CrisisDTO dto : list) {
+                crisises.add(dto.toEntity());
+            }
+            return crisises;
+        }
+        return null;
+    }
 
-	public CrisisType toEntity() throws PropertyNotSetException {
-		CrisisType cType = new CrisisType(this.getName());
-		if (this.getCrisisTypeId() != null) {
-			cType.setCrisisTypeId(this.getCrisisTypeId());
-		} 
-		cType.setName(this.getName());
-		if (this.getCrisisesDTO() != null) {
-			cType.setCrisises(toCrisisList(this.getCrisisesDTO()));
-		}
+    public CrisisType toEntity() throws PropertyNotSetException {
+        CrisisType cType = new CrisisType(this.getName());
+        if (this.getCrisisTypeId() != null) {
+            cType.setCrisisTypeId(this.getCrisisTypeId());
+        }
+        cType.setName(this.getName());
+        if (this.getCrisisesDTO() != null) {
+            cType.setCrisises(toCrisisList(this.getCrisisesDTO()));
+        }
 
-		return cType;
-	}
+        return cType;
+    }
+
+    /**
+     * @return the numberOfCrisisAssociated
+     */
+    public int getNumberOfCrisisAssociated() {
+        return numberOfCrisisAssociated;
+    }
+
+    /**
+     * @param numberOfCrisisAssociated the numberOfCrisisAssociated to set
+     */
+    public void setNumberOfCrisisAssociated(int numberOfCrisisAssociated) {
+        this.numberOfCrisisAssociated = numberOfCrisisAssociated;
+    }
 }
--- aidr-db-manager/src/main/java/qa/qcri/aidr/dbmanager/ejb/remote/facade/imp/CrisisTypeResourceFacadeImp.java
+++ aidr-db-manager/src/main/java/qa/qcri/aidr/dbmanager/ejb/remote/facade/imp/CrisisTypeResourceFacadeImp.java
@@ -16,92 +16,89 @@ import org.hibernate.criterion.Restrictions;
 import qa.qcri.aidr.common.exception.PropertyNotSetException;
 import qa.qcri.aidr.dbmanager.dto.CrisisDTO;
 import qa.qcri.aidr.dbmanager.dto.CrisisTypeDTO;
-import qa.qcri.aidr.dbmanager.dto.DocumentNominalLabelDTO;
 import qa.qcri.aidr.dbmanager.ejb.local.facade.impl.CoreDBServiceFacadeImp;
 import qa.qcri.aidr.dbmanager.ejb.remote.facade.CrisisTypeResourceFacade;
 import qa.qcri.aidr.dbmanager.entities.misc.Crisis;
 import qa.qcri.aidr.dbmanager.entities.misc.CrisisType;
-import qa.qcri.aidr.dbmanager.entities.task.Document;
-import qa.qcri.aidr.dbmanager.entities.task.DocumentNominalLabel;
 
-@Stateless(name="CrisisTypeResourceFacadeImp")
+@Stateless(name = "CrisisTypeResourceFacadeImp")
 public class CrisisTypeResourceFacadeImp extends CoreDBServiceFacadeImp<CrisisType, Long> implements CrisisTypeResourceFacade {
 
-	private Logger logger = Logger.getLogger("db-manager-log");
+    private Logger logger = Logger.getLogger("db-manager-log");
 
-	public CrisisTypeResourceFacadeImp() {
-		super(CrisisType.class);
-	}
-	
-	@Override
-	public List<CrisisTypeDTO> getAllCrisisTypes() throws PropertyNotSetException {
-		List<CrisisTypeDTO> crisisTypeDTOList = new ArrayList<CrisisTypeDTO>();
-		List<CrisisType> crisisTypeList = getAll();
-		for (CrisisType cType: crisisTypeList) {
-			crisisTypeDTOList.add(new CrisisTypeDTO(cType));
-		}
-		return crisisTypeDTOList;
-	}
+    public CrisisTypeResourceFacadeImp() {
+        super(CrisisType.class);
+    }
 
-	@Override
-	public CrisisTypeDTO addCrisisType(CrisisTypeDTO crisisType) throws PropertyNotSetException {
-		em.persist(crisisType.toEntity());
-		return findByCriteria("name", crisisType.getName()).get(0);
-	}
+    @Override
+    public List<CrisisTypeDTO> getAllCrisisTypes() throws PropertyNotSetException {
+        List<CrisisTypeDTO> crisisTypeDTOList = new ArrayList<CrisisTypeDTO>();
+        List<CrisisType> crisisTypeList = getAll();
+        for (CrisisType cType : crisisTypeList) {
+            Hibernate.initialize(cType.getCrisises());
+            crisisTypeDTOList.add(new CrisisTypeDTO(cType));
+        }
+        return crisisTypeDTOList;
+    }
 
-	@Override
-	public CrisisTypeDTO editCrisisType(CrisisTypeDTO crisisType) throws PropertyNotSetException {
-		CrisisType newCrisisType = em.merge(crisisType.toEntity());
-		return new CrisisTypeDTO(newCrisisType);
-	}
+    @Override
+    public CrisisTypeDTO addCrisisType(CrisisTypeDTO crisisType) throws PropertyNotSetException {
+        em.persist(crisisType.toEntity());
+        return findByCriteria("name", crisisType.getName()).get(0);
+    }
 
-	@Override
-	public Integer deleteCrisisType(Long id) {
-		CrisisType crisisType = getById(id);
-		if (crisisType != null) {
-			this.delete(crisisType);
-			return 1;
-		}
-		else {
-			throw new RuntimeException("CrisisType requested to be deleted does not exist! id = " + id);
-		}
-	}
+    @Override
+    public CrisisTypeDTO editCrisisType(CrisisTypeDTO crisisType) throws PropertyNotSetException {
+        CrisisType newCrisisType = em.merge(crisisType.toEntity());
+        return new CrisisTypeDTO(newCrisisType);
+    }
 
-	@Override
-	public List<CrisisTypeDTO> findByCriteria(String columnName, Object value) throws PropertyNotSetException {
-		List<CrisisType> list = getAllByCriteria(Restrictions.eq(columnName,value));
-		List<CrisisTypeDTO> dtoList = new ArrayList<CrisisTypeDTO>();
-		if (list != null) {
-			for (CrisisType c: list) {
-				dtoList.add(new CrisisTypeDTO(c));
-			}
-		}
-		return dtoList;
-	}
+    @Override
+    public Integer deleteCrisisType(Long id) {
+        CrisisType crisisType = getById(id);
+        if (crisisType != null) {
+            this.delete(crisisType);
+            return 1;
+        } else {
+            throw new RuntimeException("CrisisType requested to be deleted does not exist! id = " + id);
+        }
+    }
 
-	@Override
-	public CrisisTypeDTO findCrisisTypeByID(Long id) throws PropertyNotSetException {
-		CrisisType c = getById(id);
-		return c != null ? new CrisisTypeDTO(c) : null;
-	}
+    @Override
+    public List<CrisisTypeDTO> findByCriteria(String columnName, Object value) throws PropertyNotSetException {
+        List<CrisisType> list = getAllByCriteria(Restrictions.eq(columnName, value));
+        List<CrisisTypeDTO> dtoList = new ArrayList<CrisisTypeDTO>();
+        if (list != null) {
+            for (CrisisType c : list) {
+                dtoList.add(new CrisisTypeDTO(c));
+            }
+        }
+        return dtoList;
+    }
 
-	@Override
-	public boolean isCrisisTypeExists(Long id) throws PropertyNotSetException {
-		CrisisType c = getById(id);
-		return c != null ? true : false;
-	}
+    @Override
+    public CrisisTypeDTO findCrisisTypeByID(Long id) throws PropertyNotSetException {
+        CrisisType c = getById(id);
+        return c != null ? new CrisisTypeDTO(c) : null;
+    }
 
-	@Override
-	public List<CrisisDTO> getAllCrisisForCrisisTypeID(Long id) throws PropertyNotSetException {
-		List<CrisisDTO> dtoList = new ArrayList<CrisisDTO>();
-		CrisisType crisisType = this.getById(id);
-		if (crisisType != null) {
-			Hibernate.initialize(crisisType.getCrisises());
-			for (Crisis c: crisisType.getCrisises()) {
-				dtoList.add(new CrisisDTO(c));
-			}
-		}
-		return dtoList;
-	}
-	   
+    @Override
+    public boolean isCrisisTypeExists(Long id) throws PropertyNotSetException {
+        CrisisType c = getById(id);
+        return c != null ? true : false;
+    }
+
+    @Override
+    public List<CrisisDTO> getAllCrisisForCrisisTypeID(Long id) throws PropertyNotSetException {
+        List<CrisisDTO> dtoList = new ArrayList<CrisisDTO>();
+        CrisisType crisisType = this.getById(id);
+        if (crisisType != null) {
+            Hibernate.initialize(crisisType.getCrisises());
+            for (Crisis c : crisisType.getCrisises()) {
+                dtoList.add(new CrisisDTO(c));
+            }
+        }
+        return dtoList;
+    }
+
 }
