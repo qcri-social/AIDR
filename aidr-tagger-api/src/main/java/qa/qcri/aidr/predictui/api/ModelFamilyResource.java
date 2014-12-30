35776dd29f66db238155f99e3a2c81b23216ef9f
imran15 <mimran15@gmail.com>
2014-12-30 12:23:41 +0300

    Tagger-api now sends proper response to manager for addnewclassifer

--- aidr-tagger-api/src/main/java/qa/qcri/aidr/predictui/api/ModelFamilyResource.java
+++ aidr-tagger-api/src/main/java/qa/qcri/aidr/predictui/api/ModelFamilyResource.java
@@ -41,118 +41,124 @@ import static qa.qcri.aidr.predictui.util.ConfigProperties.getProperty;
 @Stateless
 public class ModelFamilyResource {
 
-	@Context
-	private UriInfo context;
-	@EJB
-	private ModelFamilyFacade modelFamilyLocalEJB;
+    @Context
+    private UriInfo context;
+    @EJB
+    private ModelFamilyFacade modelFamilyLocalEJB;
 
-	//private static Logger logger = Logger.getLogger(ModelFamilyResource.class);
-	private static Logger logger = Logger.getLogger("aidr-tagger-api");
-	private static ErrorLog elog = new ErrorLog();
+    //private static Logger logger = Logger.getLogger(ModelFamilyResource.class);
+    private static Logger logger = Logger.getLogger("aidr-tagger-api");
+    private static ErrorLog elog = new ErrorLog();
 
-	public ModelFamilyResource() {
-	}
-
-	@GET
-	@Produces("application/json")
-	@Path("/all")
-	public Response getAllModelFamilies() {
-		List<ModelFamilyDTO> modelFamilyList = modelFamilyLocalEJB.getAllModelFamilies();
-		ResponseWrapper response = new ResponseWrapper();
-		response.setMessage("SUCCESS");
-		response.setModelFamilies(modelFamilyList);
-		return Response.ok(response).build();
-	}
-
-	@GET
-	@Produces("application/json")
-	@Path("/crisis/{id}")
-	public Response getAllModelFamiliesByCrisisID(@PathParam("id") Long crisisID) {
-		List<ModelFamilyDTO> modelFamilyList = modelFamilyLocalEJB.getAllModelFamiliesByCrisis(crisisID);
-		ResponseWrapper response = new ResponseWrapper();
-		response.setMessage("SUCCESS");
-		response.setModelFamilies(modelFamilyList);
-		return Response.ok(response).build();
-	}
-
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("{id}")
-	public Response getModelByID(@PathParam("id") Long id){
-		ModelFamilyDTO modelFamily = modelFamilyLocalEJB.getModelFamilyByID(id);
-		return Response.ok(modelFamily).build();
-	}
-
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response addCrisisAttribute(ModelFamilyDTO modelFamilyDTO) {
-		//      because ModelFamily has @XmlTransient annotation for crises and crisis was always null
-		System.out.println("Received request to add attribute = " + modelFamilyDTO.getNominalAttributeDTO().getNominalAttributeId() + " for crisis = " + modelFamilyDTO.getCrisisDTO().getCrisisID());
-		try {
-			boolean retval = modelFamilyLocalEJB.addCrisisAttribute(modelFamilyDTO);
-			System.out.println("addedCrisisAttribute = " + retval);
-			if (retval) {
-				List<ModelFamilyDTO> dtoList = modelFamilyLocalEJB.getAllModelFamiliesByCrisis(modelFamilyDTO.getCrisisDTO().getCrisisID());
-				if (dtoList != null) {
-					System.out.println("fetched list size for crisisID " + modelFamilyDTO.getCrisisDTO().getCrisisID() + " = " + dtoList.size());
-				}
-				for (ModelFamilyDTO mf: dtoList) {
-					System.out.println("Looking at: crisisID = " + mf.getCrisisDTO().getCrisisID() + ", " + mf.getNominalAttributeDTO().getNominalAttributeId());
-					if (mf.getCrisisDTO().getCrisisID().equals(modelFamilyDTO.getCrisisDTO().getCrisisID()) 
-							&& mf.getNominalAttributeDTO().getNominalAttributeId().equals(modelFamilyDTO.getNominalAttributeDTO().getNominalAttributeId())) {
-						System.out.println("Found added modelFamily = " + mf.getModelFamilyId() + " for crisis = " + modelFamilyDTO.getCrisisDTO().getCrisisID());
-						return Response.ok(mf.getModelFamilyId()).build();
-					}
-				}
-			} 
-			return Response.ok(new Long(-1)).build();
-		} catch (RuntimeException e) {
-			System.out.println("Error while adding Crisis attribute. Possible causes could be duplication of primary key, incomplete data, incompatible data format: " + modelFamilyDTO.getCrisisDTO().getCode() + "," + modelFamilyDTO.getNominalAttributeDTO().getCode());
-			logger.error(elog.toStringException(e));
-			return Response.ok(new Long(-1)).build();
-		}
-	}
-
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/taggers-by-codes")
-	public Response getTaggersByCodes(TaggersForCodesRequest codesRequest) {
-		List<TaggersForCodes> taggersByCodes = modelFamilyLocalEJB.getTaggersByCodes(codesRequest.getCodes());
-		if (taggersByCodes.isEmpty() || taggersByCodes == null){
-			taggersByCodes = Collections.emptyList();
-		}
-		ResponseWrapper response = new ResponseWrapper();
-		response.setTaggersForCodes(taggersByCodes);
-		return Response.ok(response).build();
-	}
-
-	/*
-    private ModelFamily transformModelFamilyDTO(ModelFamilyDTO modelFamilyDTO){
-        ModelFamily modelFamily = new ModelFamily();
-        modelFamily.setCrisis(modelFamilyDTO.getCrisis());
-        modelFamily.setNominalAttribute(modelFamilyDTO.getNominalAttribute());
-        modelFamily.setIsActive(modelFamilyDTO.getIsActive());
-        return modelFamily;
+    public ModelFamilyResource() {
     }
-	 */
 
-	@DELETE
-	@Path("{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response deleteAttribute(@PathParam("id") Long modelFamilyID) {
-		try {
-			modelFamilyLocalEJB.deleteModelFamily(modelFamilyID);
-		} catch (RuntimeException e) {
-			logger.error("Error while deleting Classifier for modelFamily: " + modelFamilyID);
-			logger.error(elog.toStringException(e));
+    @GET
+    @Produces("application/json")
+    @Path("/all")
+    public Response getAllModelFamilies() {
+        List<ModelFamilyDTO> modelFamilyList = modelFamilyLocalEJB.getAllModelFamilies();
+        ResponseWrapper response = new ResponseWrapper();
+        response.setMessage("SUCCESS");
+        response.setModelFamilies(modelFamilyList);
+        return Response.ok(response).build();
+    }
 
-			return Response.ok(
-					new ResponseWrapper(getProperty("STATUS_CODE_FAILED"),
-							"Error while deleting Classifier.")).build();
-		}
-		return Response.ok(new ResponseWrapper(getProperty("STATUS_CODE_SUCCESS"))).build();
-	}
+    @GET
+    @Produces("application/json")
+    @Path("/crisis/{id}")
+    public Response getAllModelFamiliesByCrisisID(@PathParam("id") Long crisisID) {
+        List<ModelFamilyDTO> modelFamilyList = modelFamilyLocalEJB.getAllModelFamiliesByCrisis(crisisID);
+        ResponseWrapper response = new ResponseWrapper();
+        response.setMessage("SUCCESS");
+        response.setModelFamilies(modelFamilyList);
+        return Response.ok(response).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public Response getModelByID(@PathParam("id") Long id) {
+        ModelFamilyDTO modelFamily = modelFamilyLocalEJB.getModelFamilyByID(id);
+        return Response.ok(modelFamily).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addCrisisAttribute(ModelFamilyDTO modelFamilyDTO) {
+        //      because ModelFamily has @XmlTransient annotation for crises and crisis was always null
+        ResponseWrapper response = new ResponseWrapper();
+        System.out.println("Received request to add attribute = " + modelFamilyDTO.getNominalAttributeDTO().getNominalAttributeId() + " for crisis = " + modelFamilyDTO.getCrisisDTO().getCrisisID());
+        try {
+            boolean retval = modelFamilyLocalEJB.addCrisisAttribute(modelFamilyDTO);
+            System.out.println("addedCrisisAttribute = " + retval);
+            if (retval) {
+                List<ModelFamilyDTO> dtoList = modelFamilyLocalEJB.getAllModelFamiliesByCrisis(modelFamilyDTO.getCrisisDTO().getCrisisID());
+                if (dtoList != null) {
+                    System.out.println("fetched list size for crisisID " + modelFamilyDTO.getCrisisDTO().getCrisisID() + " = " + dtoList.size());
+                }
+                for (ModelFamilyDTO mf : dtoList) {
+                    System.out.println("Looking at: crisisID = " + mf.getCrisisDTO().getCrisisID() + ", " + mf.getNominalAttributeDTO().getNominalAttributeId());
+                    if (mf.getCrisisDTO().getCrisisID().equals(modelFamilyDTO.getCrisisDTO().getCrisisID())
+                            && mf.getNominalAttributeDTO().getNominalAttributeId().equals(modelFamilyDTO.getNominalAttributeDTO().getNominalAttributeId())) {
+                        System.out.println("Found added modelFamily = " + mf.getModelFamilyId() + " for crisis = " + modelFamilyDTO.getCrisisDTO().getCrisisID());
+                        response.setMessage("Successfully added.");
+                        response.setEntityID(mf.getModelFamilyId());
+                        return Response.ok(response).build();
+                    }
+                }
+            }
+            response.setMessage("Failed to add.");
+            response.setEntityID(new Long(-1));
+            return Response.ok(response).build();
+        } catch (RuntimeException e) {
+            System.out.println("Error while adding Crisis attribute. Possible causes could be duplication of primary key, incomplete data, incompatible data format: " + modelFamilyDTO.getCrisisDTO().getCode() + "," + modelFamilyDTO.getNominalAttributeDTO().getCode());
+            logger.error(elog.toStringException(e));
+            response.setMessage("Failed to add.");
+            response.setEntityID(new Long(-1));
+            return Response.ok(response).build();
+        }
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/taggers-by-codes")
+    public Response getTaggersByCodes(TaggersForCodesRequest codesRequest) {
+        List<TaggersForCodes> taggersByCodes = modelFamilyLocalEJB.getTaggersByCodes(codesRequest.getCodes());
+        if (taggersByCodes.isEmpty() || taggersByCodes == null) {
+            taggersByCodes = Collections.emptyList();
+        }
+        ResponseWrapper response = new ResponseWrapper();
+        response.setTaggersForCodes(taggersByCodes);
+        return Response.ok(response).build();
+    }
+
+    /*
+     private ModelFamily transformModelFamilyDTO(ModelFamilyDTO modelFamilyDTO){
+     ModelFamily modelFamily = new ModelFamily();
+     modelFamily.setCrisis(modelFamilyDTO.getCrisis());
+     modelFamily.setNominalAttribute(modelFamilyDTO.getNominalAttribute());
+     modelFamily.setIsActive(modelFamilyDTO.getIsActive());
+     return modelFamily;
+     }
+     */
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteAttribute(@PathParam("id") Long modelFamilyID) {
+        try {
+            modelFamilyLocalEJB.deleteModelFamily(modelFamilyID);
+        } catch (RuntimeException e) {
+            logger.error("Error while deleting Classifier for modelFamily: " + modelFamilyID);
+            logger.error(elog.toStringException(e));
+
+            return Response.ok(
+                    new ResponseWrapper(getProperty("STATUS_CODE_FAILED"),
+                            "Error while deleting Classifier.")).build();
+        }
+        return Response.ok(new ResponseWrapper(getProperty("STATUS_CODE_SUCCESS"))).build();
+    }
 
 }
