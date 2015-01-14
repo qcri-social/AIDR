a26301862ba6526d60e776b66fa9c24dbec82596
ananich <anton.ananich@inpun.com>
2015-01-14 16:33:39 +0300

    Missing Predicate implementation for Java 7

---
+++ aidr-collector/src/main/java/qa/qcri/aidr/collector/java7/Predicate.java
@@ -0,0 +1,13 @@
+package qa.qcri.aidr.collector.java7;
+
+/**
+ * This class needs to be replaced with java.util.function.Predicate<T>
+ * which is introduced in Java 8
+ * @author Anthony Ananich <anton.ananich@inpun.com>
+ */
+@Deprecated
+public interface Predicate<T> {
+
+    boolean test(T t);
+
+}
