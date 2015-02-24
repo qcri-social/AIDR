4103a629f2f166eb5cd18abddc51c0ec1b413e57
jikimlucas <jikimlucas@me.com>
2015-02-24 19:56:22 +0300

    fix MicroMappers url

--- aidr-trainer-api/src/main/java/qa/qcri/aidr/trainer/api/store/URLReference.java
+++ aidr-trainer-api/src/main/java/qa/qcri/aidr/trainer/api/store/URLReference.java
@@ -11,7 +11,7 @@ public class URLReference {
 
     //public static String PUBLIC_LINK = "http://clickers.micromappers.org/aidr/";
      // for direct connection to api service.
-    public static String PUBLIC_LINK = "http://pybossa-dev.qcri.org/AIDRCrowdsourcingAPI/crisisContent.html?code=";
+    public static String PUBLIC_LINK = "http://clickers.micromappers.org/aidr/crisisContent.html?code=";
    //  public static String PUBLIC_LINK = "http://94.125.228.197:1055/AIDRTrainerAPI/crisisContent.html?code=";
 
      public static String PUBLIC_PYBOSSA_URL = "http://clickers.micromappers.org";
