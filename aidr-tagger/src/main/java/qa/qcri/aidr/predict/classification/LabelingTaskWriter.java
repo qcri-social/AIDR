701eb472cf5793b0daac64d5ce21a4131112f796
Koushik Sinha <koushik.sinha@gmail.com>
2015-04-28 02:47:40 +0300

    bug fixes to truncate documents table

--- aidr-tagger/src/main/java/qa/qcri/aidr/predict/classification/LabelingTaskWriter.java
+++ aidr-tagger/src/main/java/qa/qcri/aidr/predict/classification/LabelingTaskWriter.java
@@ -27,8 +27,7 @@ import static qa.qcri.aidr.predict.common.ConfigProperties.getProperty;
  */
 public class LabelingTaskWriter extends PipelineProcess {
 	private static Logger logger = Logger.getLogger(LabelingTaskWriter.class);
-	private static ErrorLog elog = new ErrorLog();
-	
+
 	class DocumentHistory {
 		LinkedList<WordSet> recentWordVectors = new LinkedList<WordSet>();
 		int bufferSize = 50;
@@ -55,19 +54,20 @@ public class LabelingTaskWriter extends PipelineProcess {
 		}
 	}
 
-	long lastDBWrite = 0;
-	long lastTruncateTime = 0;
+	private long lastDBWrite = 0;
+	private long lastTruncateTime = 0;
 	public long writeCount = 0;
 	ArrayList<Document> writeBuffer = new ArrayList<Document>();
 
 	// Maintain a hash table of currently seen <key> = crisisIDs that are
 	// being saved to the DB, with <value> being the number of items per crisisID
 	// being saved to the DB. 
-	Map<Integer, Long> activeCrisisIDList = new HashMap<Integer, Long>();	
-	Map<Integer, Double>activeCrisisMaxConf = new HashMap<Integer, Double>();
-
+	static Map<Integer, Long> activeCrisisIDList = new HashMap<Integer, Long>();	
+	static Map<Integer, Double> activeCrisisMaxConf = new HashMap<Integer, Double>();
+	static Map<Integer, Long> crisisLastTruncateTime = new HashMap<Integer, Long>();
+	
 	RateLimiter taskRateLimiter = new RateLimiter(
-            Integer.parseInt(getProperty("max_new_tasks_per_minute")));
+			Integer.parseInt(getProperty("max_new_tasks_per_minute")));
 	DocumentHistory history = new DocumentHistory();
 
 	protected void processItem(Document item) {
@@ -89,13 +89,12 @@ public class LabelingTaskWriter extends PipelineProcess {
 
 	void save(Document item) {
 		writeBuffer.add(item);
-		Long currentCrisisIDItemCount =  
-				activeCrisisIDList.containsKey(item.getCrisisID()) ? 
-						activeCrisisIDList.get(item.getCrisisID()) : 0L;
-						activeCrisisIDList.put(item.getCrisisID().intValue(), currentCrisisIDItemCount+1);
-						if (!isWriteRateLimited()) {
-							writeToDB();
-						}
+		Long currentCrisisIDItemCount = activeCrisisIDList.containsKey(item.getCrisisID().intValue()) ? 
+											activeCrisisIDList.get(item.getCrisisID().intValue()) : 0L;
+		activeCrisisIDList.put(item.getCrisisID().intValue(), ++currentCrisisIDItemCount);
+		if (!isWriteRateLimited()) {
+			writeToDB();
+		}
 	}
 
 	@Override
@@ -105,34 +104,23 @@ public class LabelingTaskWriter extends PipelineProcess {
 	}
 
 	void writeToDB() {
-		// log(LogLevel.INFO, "Writing " + writeBuffer.size() +
-		// " tasks/labeled items from pipeline to DB");
-		/*
-		try {
-			if (null == DataStore.taskManager) DataStore.initTaskManager();
-		} catch (Exception e) {
-			logger.error("Unable to initialize taskManager: " + DataStore.taskManager);
-			logger.error(elog.toStringException(e));
-		}*/
-		
-		
 		DataStore.saveDocumentsToDatabase(writeBuffer);
 		writeCount += writeBuffer.size(); 
 		writeBuffer.clear();
-		//DataStore
-		//        .truncateLabelingTaskBuffer(Config.LABELING_TASK_BUFFER_MAX_LENGTH);
-		if (!isTruncateRunLimited()) {
-			for (Iterator<Map.Entry<Integer, Long>> it = activeCrisisIDList.entrySet().iterator();it.hasNext(); ) {
-				Map.Entry<Integer, Long> entry = it.next();
-				int crisisID = entry.getKey();
-				if (!isTruncateRateLimited() || 
+
+		if (!isTruncateRunLimited() || 0 == lastDBWrite) {
+			for (Integer crisisID: activeCrisisIDList.keySet()) {
+				logger.info("Looking at possible truncation for crisisID = " + crisisID 
+							+ "last save count = " + activeCrisisIDList.get(crisisID) 
+							+ " [" + Integer.parseInt(getProperty("max_new_tasks_per_minute")) + "]");
+				if (!isTruncateRateLimited(crisisID) || 
 						activeCrisisIDList.get(crisisID) > Integer.parseInt(getProperty("max_new_tasks_per_minute"))) {
-					System.out.println("[writeToDB] Going to truncate for crisisID = " + crisisID + " [" + activeCrisisIDList.get(crisisID) + "] new docs");
-					logger.info("Going to truncate for crisisID = " + crisisID + " [" + activeCrisisIDList.get(crisisID) + "] new docs");
+
+					logger.info("Going to truncate for crisisID = " + crisisID 
+								+ " [" + activeCrisisIDList.get(crisisID) + "] new docs");
 					DataStore
 					.truncateLabelingTaskBufferForCrisis(crisisID, Integer.parseInt(getProperty("labeling_task_buffer_max_length")));
-					lastTruncateTime = System.currentTimeMillis();
-					it.remove();
+					activeCrisisIDList.put(crisisID, 0L);	// reset count for next interval
 					try {
 						Thread.sleep(200);
 					} catch (InterruptedException e) {}
@@ -147,11 +135,16 @@ public class LabelingTaskWriter extends PipelineProcess {
 		return (System.currentTimeMillis() - lastDBWrite) < Integer.parseInt(getProperty("max_task_write_fq_ms"));
 	}
 
-	boolean isTruncateRateLimited() {
-		return (System.currentTimeMillis() - lastTruncateTime) < Integer.parseInt(getProperty("min_truncate_interval_ms"));
+	boolean isTruncateRateLimited(int crisisID) {
+		Long lastTruncateTime = crisisLastTruncateTime.containsKey(crisisID) ? crisisLastTruncateTime.get(crisisID) : 0L;
+		boolean result = (System.currentTimeMillis() - lastTruncateTime) < Integer.parseInt(getProperty("min_truncate_interval_ms"));
+		if (!result) crisisLastTruncateTime.put(crisisID, System.currentTimeMillis());
+		return result;
 	}
 	
 	boolean isTruncateRunLimited() {
-		return (System.currentTimeMillis() - lastTruncateTime) < Long.parseLong(getProperty("truncate_run_interval_ms"));
+		boolean result = (System.currentTimeMillis() - lastTruncateTime) < Long.parseLong(getProperty("truncate_run_interval_ms"));
+		if (!result) lastTruncateTime = System.currentTimeMillis();
+		return result;
 	}
 }
