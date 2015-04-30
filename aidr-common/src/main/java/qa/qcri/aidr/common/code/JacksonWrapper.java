2a78eebaff44cace9eb00122ce5dcdf089d79273
Carlos Castillo <chato@chato.cl>
2015-04-30 14:22:34 +0300

    Added method-level documentation.

--- aidr-common/src/main/java/qa/qcri/aidr/common/code/JacksonWrapper.java
+++ aidr-common/src/main/java/qa/qcri/aidr/common/code/JacksonWrapper.java
@@ -3,8 +3,17 @@ package qa.qcri.aidr.common.code;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
 
+/**
+ * A wrapper around an {@link ObjectMapper}.
+ * 
+ */
 public class JacksonWrapper {
 	
+	/**
+	 * Generates an {@link ObjectMapper} and configures it so that it does not fail on unknown properties.
+	 * 
+	 * @return an object mapper.
+	 */
 	public static ObjectMapper getObjectMapper() {
 		try {
 		ObjectMapper objectMapper = new ObjectMapper();
