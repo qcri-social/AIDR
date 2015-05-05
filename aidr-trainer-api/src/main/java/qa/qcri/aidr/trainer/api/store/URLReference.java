b5e38126e2e8e53e52b25154c10cd99478fcd6a0
jikimlucas <jikimlucas@me.com>
2015-05-05 19:48:50 +0300

    public url update

--- aidr-trainer-api/src/main/java/qa/qcri/aidr/trainer/api/store/URLReference.java
+++ aidr-trainer-api/src/main/java/qa/qcri/aidr/trainer/api/store/URLReference.java
@@ -11,10 +11,10 @@ public class URLReference {
 
     //public static String PUBLIC_LINK = "http://clickers.micromappers.org/aidr/";
      // for direct connection to api service.
-    public static String PUBLIC_LINK = "http://clickers.micromappers.org/aidr/crisisContent.html?code=";
+    //public static String PUBLIC_LINK = "http://clickers.micromappers.org/aidr/crisisContent.html?code=";
    //  public static String PUBLIC_LINK = "http://94.125.228.197:1055/AIDRTrainerAPI/crisisContent.html?code=";
-
-     public static String PUBLIC_PYBOSSA_URL = "http://clickers.micromappers.org";
+    public static String PUBLIC_LINK ="http://aidr-prod.qcri.org/AIDRTrainerAPI/crisisContent.html?code=";
+    public static String PUBLIC_PYBOSSA_URL = "http://clickers.micromappers.org";
      //http://pybossa-dev.qcri.org/
     //public static String PUBLIC_PYBOSSA_URL = "http://pybossa-dev.qcri.org";
 
