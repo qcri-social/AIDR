976d2d40883a349c748c252f7a74a85c6fd8aca8
root <meghna.singh@yahoo.com>
2015-05-13 11:53:25 +0300

    edited 2 shortcode fields to allow only alphanumeric and underscore

--- aidr-manager/src/main/webapp/resources/js/taggui/new-custom-attribute/view/NewCustomAttributePanel.js
+++ aidr-manager/src/main/webapp/resources/js/taggui/new-custom-attribute/view/NewCustomAttributePanel.js
@@ -33,8 +33,8 @@ Ext.define('TAGGUI.new-custom-attribute.view.NewCustomAttributePanel', {
             emptyText: 'e.g. donationtype',
             maxLength: 64,
             maxLengthText: 'The maximum length for this field is 64 ',
-            maskRe: /[A-za-z0-9_]/,
-    	    stripCharsRe: /[^A-za-z0-9_]/
+            maskRe: /[\w]/,
+    	    stripCharsRe: /[^\w]/
         });
 
         this.nameE = Ext.create('Ext.form.field.Text', {
@@ -62,7 +62,8 @@ Ext.define('TAGGUI.new-custom-attribute.view.NewCustomAttributePanel', {
             emptyText: 'e.g. shoesdonation',
             maxLength: 64,
             maxLengthText: 'The maximum length for this field is 64 ',
-            maskRe: /[^ ]/
+            maskRe: /[\w]/,
+    	    stripCharsRe: /[^\w]/
         });
 
         this.nameLabelE = Ext.create('Ext.form.field.Text', {
