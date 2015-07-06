5ab8d50405426802bca4e090a1ffd4f1ebeab6e9
Koushik Sinha <koushik.sinha@gmail.com>
2015-07-06 18:12:58 +0300

    refactored code to make testing ready

---
+++ aidr-analytics/src/main/java/qa/qcri/aidr/analysis/service/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides the middle-layer glue to call the different facade implementations and aggregate
+ * results before returning the final data to the REST API layer.
+ */
+/**
+ * @author koushik
+ *
+ */
+package qa.qcri.aidr.analysis.service;
