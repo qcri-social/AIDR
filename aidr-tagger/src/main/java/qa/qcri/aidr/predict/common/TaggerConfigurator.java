22faeb50901b2f15d00967920ef7dac9f3f9f8ff
Kushal Kant Goyal <kushalkant.goyal@metacube.com>
2015-07-28 13:47:21 +0530

    Minor changes

--- aidr-tagger/src/main/java/qa/qcri/aidr/predict/common/TaggerConfigurator.java
+++ aidr-tagger/src/main/java/qa/qcri/aidr/predict/common/TaggerConfigurator.java
@@ -1,12 +1,9 @@
 package qa.qcri.aidr.predict.common;
 
-import java.io.File;
-
 import org.apache.log4j.Logger;
 
 import qa.qcri.aidr.common.code.Configurator;
 import qa.qcri.aidr.common.code.impl.BaseConfigurator;
-import qa.qcri.aidr.common.exception.DirectoryNotWritableException;
 
 /**
  * User: yakubenkova.elena@gmail.com Date: 29.09.14
@@ -24,19 +21,10 @@ public class TaggerConfigurator extends BaseConfigurator {
 		LOGGER.info("Instantiating TaggerConfigurator,");
 		this.initProperties(configLoadFileName, TaggerConfigurationProperty.values());
 		
-		this.directoryIsWritable(TaggerConfigurationProperty.MODEL_STORE_PATH);
+		this.directoryIsWritable(TaggerConfigurationProperty.MODEL_STORE_PATH.getName());
 	}
 
 	public static Configurator getInstance() {
 		return instance;
-	}
-
-	private void directoryIsWritable(TaggerConfigurationProperty propertyName) throws DirectoryNotWritableException{
-		String directoryLocation = this.getProperty(propertyName);
-		File f = new File(directoryLocation);
-		if(!f.canWrite()) {
-			LOGGER.info(propertyName.getName()+ " = " +directoryLocation+ " is not writable. Please verify if this is a valid writable directory.");
-			throw new DirectoryNotWritableException(propertyName.getName(), directoryLocation);
-		}
 	}
 }
