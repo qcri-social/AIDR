f36c9b8c4ec45db7b5a4daa5a482fec4b0550247
jikimlucas <jikimlucas@me.com>
2015-08-27 15:04:26 +0300

    update Pybossa app url to project url corresponding to pybosaa api update

--- aidr-trainer-api/src/main/java/qa/qcri/aidr/trainer/api/store/URLReference.java
+++ aidr-trainer-api/src/main/java/qa/qcri/aidr/trainer/api/store/URLReference.java
@@ -9,17 +9,11 @@ package qa.qcri.aidr.trainer.api.store;
  */
 public class URLReference {
 
-    //public static String PUBLIC_LINK = "http://clickers.micromappers.org/aidr/";
-     // for direct connection to api service.
-    //public static String PUBLIC_LINK = "http://clickers.micromappers.org/aidr/crisisContent.html?code=";
-   //  public static String PUBLIC_LINK = "http://94.125.228.197:1055/AIDRTrainerAPI/crisisContent.html?code=";
     public static String PUBLIC_LINK ="http://aidr-prod.qcri.org/AIDRTrainerAPI/crisisContent.html?code=";
     public static String PUBLIC_PYBOSSA_URL = "http://clickers.micromappers.org";
-     //http://pybossa-dev.qcri.org/
-    //public static String PUBLIC_PYBOSSA_URL = "http://pybossa-dev.qcri.org";
 
-    public static String PYBOSAA_APP = "/app/";
-    public static String PYBOSSA_APP_KEY ="/app?api_key=";
+    public static String PYBOSAA_APP = "/project/";
+    public static String PYBOSSA_APP_KEY ="/project?api_key=";
     public static String PYBOSSA_APP_UPDATE_KEY = "?api_key=";
 
 }
