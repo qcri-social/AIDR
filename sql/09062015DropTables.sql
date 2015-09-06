2ff9daafb7a229067d4788212cfea3696766681a
jikimlucas <jikimlucas@me.com>
2015-09-06 15:55:35 +0300

    FetchManager, predict db tables drop script.

---
+++ sql/09062015DropTables.sql
@@ -0,0 +1,5 @@
+DROP TABLE `aidr_fetch_manager`.`AIDR_MASTER`;
+DROP TABLE `aidr_fetch_manager`.`TASK_COLLECTION_MAPPING`;
+DROP TABLE `aidr_fetch_manager`.`AIDR_TASK`;
+DROP TABLE `aidr_predict`.`crisis_nominal_attribute`;
+DROP TABLE `aidr_predict`.`nominal_attribute_dependent_label`;
