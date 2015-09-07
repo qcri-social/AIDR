2a74469ba64d7202ec6eadadbbb101c6ae5ba52b
Kushal Kant Goyal <kushalkant.goyal@metacube.com>
2015-09-07 11:20:59 +0530

    Finishes #102155116 : Updating collection count history on the return of status NOT_FOUND from collector.

--- aidr-manager/src/main/java/qa/qcri/aidr/manager/repository/CollectionLogRepository.java
+++ aidr-manager/src/main/java/qa/qcri/aidr/manager/repository/CollectionLogRepository.java
@@ -1,11 +1,12 @@
 package qa.qcri.aidr.manager.repository;
 
-import qa.qcri.aidr.manager.dto.CollectionLogDataResponse;
-import qa.qcri.aidr.manager.hibernateEntities.AidrCollectionLog;
-
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+
+import qa.qcri.aidr.manager.dto.CollectionLogDataResponse;
+import qa.qcri.aidr.manager.hibernateEntities.AidrCollectionLog;
 
 public interface CollectionLogRepository extends GenericRepository<AidrCollectionLog, Serializable> {
 
@@ -16,6 +17,8 @@ public interface CollectionLogRepository extends GenericRepository<AidrCollectio
     public CollectionLogDataResponse getPaginatedDataForCollection(Integer start, Integer limit, Integer collectionId);
 
     public Integer countTotalDownloadedItemsForCollection(Integer collectionId);
+    
+    public Integer countLogsStartedInInterval(Integer collectionId, Date startDate, Date endDate);
 
     public Map<Integer, Integer> countTotalDownloadedItemsForCollectionIds(List<Integer> ids);
 }
