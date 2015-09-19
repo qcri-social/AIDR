46a832d0698fc074a6f7bb46db35dcdd89d43acc
Latika Bhurani <latika.bhurani@metacube.com>
2015-09-19 17:19:05 +0530

    Commit for some minor fixes for user role

--- aidr-manager/src/main/java/qa/qcri/aidr/manager/repository/impl/UserRoleRepositoryImpl.java
+++ aidr-manager/src/main/java/qa/qcri/aidr/manager/repository/impl/UserRoleRepositoryImpl.java
@@ -7,10 +7,11 @@ package qa.qcri.aidr.manager.repository.impl;
  * @author Latika
  *
  */
+import java.util.Collections;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
-import org.hibernate.criterion.Conjunction;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
@@ -23,30 +24,43 @@ import qa.qcri.aidr.manager.repository.UserRoleRepository;
 @Repository
 public class UserRoleRepositoryImpl extends GenericRepositoryImpl<UserRole, Long> implements UserRoleRepository {
 
-	@Override
-	public UserRole findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	private Logger logger = Logger.getLogger(UserRoleRepositoryImpl.class);
+	
 	@Override
 	public List<RoleType> fetchByUserId(Integer userId) {
-		Criteria criteria = getHibernateTemplate().getSessionFactory()
-				.getCurrentSession().createCriteria(UserRole.class);
-        criteria.add(Restrictions.eq("user.id", userId));
-        criteria.setProjection(Projections.property("roleType"));
-        return (List<RoleType>) criteria.list();
+		
+		List<RoleType> roles = Collections.EMPTY_LIST;
+		
+		try {
+			Criteria criteria = getHibernateTemplate().getSessionFactory()
+					.getCurrentSession().createCriteria(UserRole.class);
+	        criteria.add(Restrictions.eq("user.id", userId));
+	        criteria.setProjection(Projections.property("roleType"));
+	        roles = (List<RoleType>) criteria.list();
+		} catch (Exception e) {
+			logger.error("Error in fetching roles by userID : " + userId, e);
+		}
+		return roles;
 	}
 
 	@Override
 	public UserRole fetchByUserIdAndRoleType(Integer userId,
 			RoleType roleType) {
-		Criteria criteria = getHibernateTemplate().getSessionFactory()
-				.getCurrentSession().createCriteria(UserRole.class);
-		Criterion criterion = Restrictions.conjunction()
-				.add(Restrictions.eq("user.id", userId))
-				.add(Restrictions.eq("roleType", roleType.name()));
-        return (UserRole) criteria.uniqueResult();
+		UserRole userRole = new UserRole();
+		
+		try {
+			Criteria criteria = getHibernateTemplate().getSessionFactory()
+					.getCurrentSession().createCriteria(UserRole.class);
+			Criterion criterion = Restrictions.conjunction()
+					.add(Restrictions.eq("user.id", userId))
+					.add(Restrictions.eq("roleType", roleType));
+			criteria.add(criterion);
+		    userRole = (UserRole) criteria.uniqueResult();
+		} catch(Exception e) {
+			logger.error("Error in fetching data by userId : " + userId + " and roleType : " + roleType, e);
+		}
+		
+		return userRole;
 	}
 }
 
