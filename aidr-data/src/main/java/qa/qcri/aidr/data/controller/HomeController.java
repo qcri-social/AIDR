a5d7b5664243191141a7eda07916276ddca6a7ef
Latika Bhurani <latika.bhurani@metacube.com>
2015-11-14 16:33:18 +0530

    Commit for redirecting aidr-data to index.html : #103838828

--- aidr-data/src/main/java/qa/qcri/aidr/data/controller/HomeController.java
+++ aidr-data/src/main/java/qa/qcri/aidr/data/controller/HomeController.java
@@ -7,26 +7,38 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import qa.qcri.aidr.data.model.CollectionSummaryInfo;
 import qa.qcri.aidr.data.service.CollectionSummaryService;
 
 @Controller
-@RequestMapping("/dashboard")
-public class CollectionSummaryController {
+public class HomeController {
 
 	@Autowired
     protected CollectionSummaryService collectionSummaryService;
     
     protected static final Logger LOGGER = LoggerFactory
-            .getLogger(CollectionSummaryController.class);
+            .getLogger(HomeController.class);
     
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @RequestMapping(value = "/dashboard/list")
     @ResponseBody
     public List<CollectionSummaryInfo> list(){
     	List<CollectionSummaryInfo> list=collectionSummaryService.fetchAllCollections();
         return list;
     }
+    
+    @ResponseBody
+    @RequestMapping(value = "/dashboard")
+    public ModelAndView index(){
+    	ModelAndView view = new ModelAndView();  
+        view.setViewName("index"); //name of the jsp-file in the "page" folder  
+          
+        String str = "MVC Spring is here!";  
+        view.addObject("message", str); //adding of str object as "message" parameter  
+          
+        return view;  
+    }
+    
 }
