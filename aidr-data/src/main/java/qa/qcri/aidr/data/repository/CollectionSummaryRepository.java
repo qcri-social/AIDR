4fd060a74138570ca99831eb941ab36a88968f48
Latika Bhurani <latika.bhurani@metacube.com>
2015-11-16 16:33:43 +0530

    Commit for #103744042 : fetch collections only with non-zero tweets

--- aidr-data/src/main/java/qa/qcri/aidr/data/repository/CollectionSummaryRepository.java
+++ aidr-data/src/main/java/qa/qcri/aidr/data/repository/CollectionSummaryRepository.java
@@ -2,13 +2,20 @@ package qa.qcri.aidr.data.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import qa.qcri.aidr.data.persistence.entity.CollectionSummary;
 
 public interface CollectionSummaryRepository extends CrudRepository<CollectionSummary, Long>{
 
+	public static final String FETCH_ALL_ZERO_TWEET_COLLECTION_ORDER_BY_COLLECTION_CREATION_DATE = 
+			"SELECT col FROM CollectionSummary col WHERE col.totalCount > 0 ORDER BY col.collectionCreationDate DESC";
+	
     List<CollectionSummary> findByName(String name);
-    List<CollectionSummary> findAllByOrderByCollectionCreationDateDesc();
+    
+    @Query(FETCH_ALL_ZERO_TWEET_COLLECTION_ORDER_BY_COLLECTION_CREATION_DATE)
+    List<CollectionSummary> getCollectionsOrderByCollectionCreationDate();
+    
     CollectionSummary findByCode(String code);
 }
