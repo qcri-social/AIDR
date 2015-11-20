947422a840d8a9f95badab3a2b87a2b0493fb632
Kushal Kant Goyal <kushalkant.goyal@metacube.com>
2015-11-20 12:13:10 +0530

    Fixed Duration Hours null on create collection

--- aidr-manager/src/main/java/qa/qcri/aidr/manager/service/impl/CollectionLogServiceImpl.java
+++ aidr-manager/src/main/java/qa/qcri/aidr/manager/service/impl/CollectionLogServiceImpl.java
@@ -54,6 +54,7 @@ public class CollectionLogServiceImpl implements CollectionLogService {
     }
 
     @Override
+    @Transactional(readOnly = false)
     public void create(CollectionLog collectionLog) throws Exception {
         collectionLogRepository.save(collectionLog);
     }
