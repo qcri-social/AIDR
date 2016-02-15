9504308d163f871cf7d04c332cf67bc43b3ad59a
Aman Agrawal <aman.agrawal9@gmail.com>
2016-02-15 15:50:29 +0530

    API Changes

--- aidr-persister/src/main/java/qa/qcri/aidr/persister/api/DataFeedAPI.java
+++ aidr-persister/src/main/java/qa/qcri/aidr/persister/api/DataFeedAPI.java
@@ -1,6 +1,8 @@
 package qa.qcri.aidr.persister.api;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -8,6 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -21,28 +24,38 @@ import qa.qcri.aidr.service.DataFeedService;
 @Component
 public class DataFeedAPI {
 	
+	@SuppressWarnings("resource")
 	@GET
 	@Produces("application/json")
 	@Consumes("application/json")
-	@Path("/{code}/confidence")
-    public String findbyCollectionCodeAndConfidence(@PathParam("code") String code, 
+	@Path("/{code}/by-confidence")
+    public Response findbyCollectionCodeAndConfidence(@PathParam("code") String code, 
     		@QueryParam("confidence") double confidence, 
-    		@QueryParam("offset") double offset,
-    		@QueryParam("limit") double limit){
+    		@QueryParam("offset") Integer offset,
+    		@QueryParam("limit") Integer limit){
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("spring/spring-servlet.xml");
         DataFeedService dataFeedService = (DataFeedService) appContext.getBean("dataFeedService");
 		List<DataFeed> listOfdaDataFeeds = dataFeedService.findbyCollectionCodeAndConfidence(code, confidence, offset, limit);
-    	return "HelloWorld";
+		return Response.ok(getUIWrapper(listOfdaDataFeeds, "", true)).build();
     }
 	
+	@SuppressWarnings("resource")
 	@RequestMapping(value = "/{code}")
-    public String findbyCollectionCode(@PathParam("code") String code, 
-    		@QueryParam("offset") double offset,
-    		@QueryParam("limit") double limit){
+    public Response findbyCollectionCode(@PathParam("code") String code, 
+    		@QueryParam("offset") Integer offset,
+    		@QueryParam("limit") Integer limit){
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("spring/spring-servlet.xml");
         DataFeedService dataFeedService = (DataFeedService) appContext.getBean("dataFeedService");
 		List<DataFeed> listOfdaDataFeeds = dataFeedService.findbyCollectionCode(code, offset, limit);
-    	return "HelloWorld";
+		return Response.ok(getUIWrapper(listOfdaDataFeeds, "", true)).build();
     }
+	
+	public Map<String, Object> getUIWrapper(List<DataFeed> listOfdaDataFeeds, String message, Boolean success) {
+		Map<String, Object> modelMap = new HashMap<String, Object>(4);
+		modelMap.put("data", listOfdaDataFeeds);
+		modelMap.put("message", message);
+		modelMap.put("success", success);		
+		return modelMap;
+	}
 	
 }
