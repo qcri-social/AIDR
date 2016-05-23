f6998db8d13b8102fbc7ba0e73d0c4f8af2eb887
Latika Bhurani <latika.bhurani@metacube.com>
2016-05-23 16:23:12 +0530

    Commit for #119817665

--- aidr-db-manager/src/main/java/qa/qcri/aidr/util/NativeQueryUtil.java
+++ aidr-db-manager/src/main/java/qa/qcri/aidr/util/NativeQueryUtil.java
@@ -2,13 +2,25 @@ package qa.qcri.aidr.util;
 
 public class NativeQueryUtil {
 
-	public static final String TRAINING_COUNT_FOR_CRISIS = " select na.nominalAttributeID, na.name, "
-			+ " mf.modelFamilyID, mf.isActive, count(1) as humanTaggedCount from model_family mf "
-			+ " join nominal_attribute na on na.nominalAttributeID = mf.nominalAttributeID"
-			+ " join document d on d.crisisID = :crisisID "
-			+ " join document_nominal_label dnl on d.documentID = dnl.documentID"
-			+ " join nominal_label lbl on lbl.nominalLabelID=dnl.nominalLabelID"
-			+ " where mf.crisisID = :crisisID group by mf.nominalAttributeID;";
+	public static final String TRAINING_COUNT_FOR_CRISIS = " SELECT mf_temp.nominalAttributeID, mf_temp.name as attributeName,"
+			+ " mf_temp.modelFamilyID, mf_temp.isActive,"
+			+ " COALESCE(doc_temp.taggedCount, 0) as totalTaggedCount "
+			+ " FROM "
+				
+			+ "(SELECT mf.nominalAttributeID, mf.isActive, mf.modelFamilyID, na.name "
+			+ "FROM model_family mf, nominal_attribute na "
+			+ "WHERE crisisID = :crisisID and mf.nominalAttributeID = na.nominalAttributeID) mf_temp "
+			
+			+ "LEFT JOIN "
+			
+			+ "(SELECT nl.nominalAttributeID, count(1) AS taggedCount "
+			+ "FROM document_nominal_label dnl, document d, nominal_label nl "
+			+ "WHERE dnl.documentID = d.documentID AND d.crisisID = :crisisID and nl.nominalLabelID = dnl.nominalLabelID "
+			+ "GROUP BY nl.nominalAttributeID) doc_temp ON mf_temp.nominalAttributeID = doc_temp.nominalAttributeID "
+		
+			+ "GROUP BY mf_temp.nominalAttributeID;";
+
+;
 	
 	public static final String MODEL_DETAILS_FOR_CRISIS = "select m.modelID, m.avgAuc, m.modelFamilyID, "
 			+ "sum(mnl.classifiedDocumentCount) from model_nominal_label mnl "
